a99ef263ea177c09db7d787223729ad948dd5650
Pinki Gupta <pinkigupta0506@gmail.com>
2024-05-02 15:30:20 +0530

    date

---
+++ basics/String.js
@@ -0,0 +1,12 @@
+let s = " pinki "
+console.log(typeof s);//value is of string type 
+const game = new String( "    .omkam   ")
+console.log(game);
+console.log(game[3])//will give me index at that particular place 
+console.log(game.charAt(10));
+console.log(game.indexOf("m"))
+console.log(game.trim());
+
+
+
+
---
+++ basics/numaths.js
@@ -0,0 +1,18 @@
+// const score = 388;
+// console.log(score);
+
+// const balance = new Number(122)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+
+// const num = 163548574848035;
+// console.log(num.toPrecision(4));
+// console.log(num.toLocaleString());
+//  ****************************
+let mydate = new Date()
+console.log( mydate.toDateString())
+console.log( mydate.toJSON())
+let datecrete = 
+
