e82c9525f7c78ebe30a44bc62f0267eb3db79594
Pinki Gupta <pinkigupta0506@gmail.com>
2024-06-04 23:52:06 +0530

    commit

---
+++ basics/Stringg.js
@@ -0,0 +1,2 @@
+let a =" utir"
+console.log(a)
--- basics/numaths.js
+++ basics/numaths.js
@@ -14,5 +14,38 @@
 let mydate = new Date()
 console.log( mydate.toDateString())
 console.log( mydate.toJSON())
-let datecrete = 
+let datecrete = new Date()
+console.log(datecrete);
+let  timestamp = Date.now()
+console.log(datecrete.getMonth())
+// ************Array
+const m1 = [0,1,2,3,4,5,6,7]
+console.log(m1)
+
+// const m2 = m1.slice(1,3)
+// console.log(m2)
+// const m3 = m1.splice(1,3)
+// console.log(m3)
+// console.log(m1)
+
+
+// const books = ["scifi","horror","comic"]
+// // ******************************
+
+// let syllables 
+// [erfect 
+// schaool 
+// ]
+
+const myarr=[0,1,2,3,4,5]
+const myheroes = new Array(1,2,3,4)
+console.log(myheroes[1])
+myarr.push(100);
+myarr.pop()
+console.log(myarr)
+myarr.unshift()
+const newaa =myarr.join()
+// *******slice n splice ***********
+const marvel = ["a","b","c"]
+const marv =["f","d","e"]
 
